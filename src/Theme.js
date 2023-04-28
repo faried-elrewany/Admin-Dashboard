392441e54e65136b69714f6e397e7daf46a008f2
faried-elrewany <fariedezz4@gmail.com>
2023-04-29 02:48:09 +0300

    setup files and libraries

---
+++ src/Theme.js
@@ -0,0 +1,16 @@
+import { createContext, useState, useMemo } from "react";
+import { createTheme } from "@mui/material";
+
+// all colors in the website
+
+black: {
+  900: "#333300"
+  800: "#666600",
+  700: "#999900",
+  600: "#cccc00",
+  500: "#ffff00",
+  400: "#ffff33",
+  300: "#ffff66",
+  200: "#ffff99",
+  100: "#ffffcc",
+},
