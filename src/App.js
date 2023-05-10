ce856e2a726da8b2ead6309951a8e0b6ed220a49
faried-elrewany <fariedezz4@gmail.com>
2023-05-10 16:02:52 +0300

    update pro side bar rtl and ltr

--- src/App.js
+++ src/App.js
@@ -3,23 +3,35 @@ import { Routes, Route } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./Theme";
 import Topbar from "./Views/Global/Topbar";
-import Layout from "./Views/Global/Layout";
+import Layout from "./Views/Global/Sidebar/Layout";
 import { ProSidebarProvider } from "react-pro-sidebar";
-
+import { MyProSidebarProvider } from "./Views/Global/Sidebar/sidebarContext";
 export default function App() {
   const [theme, colorMode] = useMode();
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <div className="app">
-          <main>
-            {/* <Topbar /> */}
-            <ProSidebarProvider>
-              <Layout />
-            </ProSidebarProvider>
-          </main>
-        </div>
+        <MyProSidebarProvider>
+          <div style={{ height: "100%", width: "100%" }}>
+            <main>
+              <Topbar />
+              <Routes>
+                {/* <Route path="/" element={<Dashboard />} />
+                <Route path="/team" element={<Team />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/invoices" element={<Invoices />} />
+                <Route path="/form" element={<Form />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/line" element={<Line />} />
+                <Route path="/faq" element={<FAQ />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/geography" element={<Geography />} /> */}
+              </Routes>
+            </main>
+          </div>
+        </MyProSidebarProvider>
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
